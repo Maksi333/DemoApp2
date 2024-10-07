a322ddc3a6cbdddc63258db90d5cddb349cc2920
Maksi333 <Simon_n_andersen@live.dk>
2024-10-07 13:06:05 +0200

    Initial Commit

---
+++ DemoApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DemoApp
+//
+//  Created by dmu mac 28 on 07/10/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+                .padding(10)
+            Text("fuck")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DemoApp/DemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAppApp.swift
+//  DemoApp
+//
+//  Created by dmu mac 28 on 07/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct DemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
