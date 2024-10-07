2a978fbeb99474f7f9cf02a44410cd15807e4d7f
Maksi333 <Simon_n_andersen@live.dk>
2024-10-07 13:15:16 +0200

    Initial Commit

---
+++ DemoApp/WhatThe.swift
@@ -0,0 +1,20 @@
+//
+//  WhatThe.swift
+//  DemoApp
+//
+//  Created by dmu mac 28 on 07/10/2024.
+//
+
+import SwiftUI
+
+
+struct WhatThe: View {
+    var body: some View {
+        Text("try again")
+    }
+    
+}
+
+#Preview {
+    WhatThe()
+}
